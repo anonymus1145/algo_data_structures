11c7a8708f41fb8356a70df454bc69d3c88ed8e9
Anonymus1145 <Anonymus1145@yahoo.com>
2025-11-02 18:16:56 +0200

    Added reduce function to array manipulation

--- string_manipulation.js
+++ string_manipulation.js
@@ -17,3 +17,24 @@ function funWithAnagrams(text) {
   // Return the result sorted alphabetically
   return result.sort();
 }
+
+// Reduce array and sort articles
+const reduceArticles = (allArticles) => {
+  const validArticles = allArticles.reduce((acc, a) => {
+    const name = a.title || a.story_title;
+    if (name) {
+      acc.push({
+        name,
+        comments: typeof a.num_comments === "number" ? a.num_comments : 0
+      });
+    }
+    return acc;
+  }, []);
+
+  validArticles.sort((a, b) => {
+    if (b.comments !== a.comments) return b.comments - a.comments;
+    return b.name.localeCompare(a.name);
+  });
+
+  return validArticles.slice(0, limit).map((a) => a.name);
+}
