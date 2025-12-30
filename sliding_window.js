ac6fb328e39e7571f940a61a199f77805ad69aef
Anonymus1145 <Anonymus1145@yahoo.com>
2025-12-30 16:29:59 +0200

    Added new sliding_window

--- sliding_window.js
+++ sliding_window.js
@@ -24,3 +24,30 @@ const minAbsoluteDifference = function (nums, x) {
   }
   return val;
 };
+
+// Find the Power of K-Size Subarrays II
+// You are given an array of integers nums of length n and a positive integer k.
+// The power of an array is defined as:
+//Its maximum element if all of its elements are consecutive and sorted in ascending order.
+//-1 otherwise.
+//You need to find the power of all
+//of nums of size k.
+const resultsArray = function (nums, k) {
+  const res = [];
+  let streak = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log(streak);
+    if (i > 0 && nums[i] === nums[i - 1] + 1) {
+      streak++;
+    } else {
+      streak = 1;
+    }
+
+    if (i >= k - 1) {
+      res.push(streak >= k ? nums[i] : -1);
+    }
+  }
+
+  return res;
+};
